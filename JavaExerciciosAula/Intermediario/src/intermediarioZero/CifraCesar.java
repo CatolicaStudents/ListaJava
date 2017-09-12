6304ee46acb18b6421fb2fb6434af4ade67c93bf
CatolicaStudents <31051150+CatolicaStudents@users.noreply.github.com>
2017-09-11 21:29:08 -0300

    Add files via upload

---
+++ JavaExerciciosAula/Intermediario/src/intermediarioZero/CifraCesar.java
@@ -0,0 +1,12 @@
+package intermediarioZero;
+
+
+public class CifraCesar {
+	public static void main (String [] args){
+	
+		Criptografia cript = new Criptografia();
+		System.out.println(cript.cifar("GLAUCO"));
+		                                
+	return;
+	}
+}
